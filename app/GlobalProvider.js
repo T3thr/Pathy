48077d3b55c2100cc1c0a9950dbbd1a860154af2
T3thr <t.theerapat33@gmail.com>
2024-10-27 06:25:56 +0700

    modify

--- app/GlobalProvider.js
+++ app/GlobalProvider.js
@@ -2,13 +2,13 @@
 
 import { SessionProvider } from "next-auth/react";
 
-import { ToastContainer } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
 
 export function GlobalProvider({ children }) {
   return (
     <>
-      <ToastContainer position="bottom-right" />
+
       <SessionProvider>{children}</SessionProvider>
 
     </>
