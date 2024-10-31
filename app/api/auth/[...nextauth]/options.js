4d8b1dcd1fcd5ce4ff0a81aadc08b8690e343d93
T3thr <t.theerapat33@gmail.com>
2024-11-01 01:43:41 +0700

    m

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -18,21 +18,17 @@ export const options = {
             },
             async authorize(credentials, req) {
                 await mongodbConnect();
-            
+                let user;
+
                 // Check for email sign-in
                 if (credentials.email) {
-                    const user = await User.findOne({ email: credentials.email }).select("+password");
-            
-                    if (!user) {
-                        throw new Error("No user found with this email");
-                    }
-            
-                    const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
-            
-                    if (!isPasswordValid) {
-                        throw new Error("Incorrect password");
-                    }
-            
+                    user = await User.findOne({ email: credentials.email }).select("+password");
+                } else if (credentials.username) {
+                    // Check for username sign-in
+                    user = await User.findOne({ username: credentials.username }).select("+password");
+                }
+
+                if (user && await bcrypt.compare(credentials.password, user.password)) {
                     // Log login activity
                     const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress;
                     await LoginActivity.create({
@@ -42,62 +38,38 @@ export const options = {
                         username: user.username,
                         ipAddress: ipAddress,
                     });
-            
-                    return { id: user._id, ...user.toObject() };
+                    
+                    // Update lastLogin
+                    user.lastLogin = Date.now();
+                    await user.save();
+
+                    return { id: user._id, name: user.name, username: user.username, email: user.email, role: user.role };
                 }
-            
-                // Check for username sign-in
-                const user = await User.findOne({ username: credentials.username }).select("+password");
-            
-                if (user) {
-                    const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
-            
-                    if (!isPasswordValid) {
-                        throw new Error("Incorrect password");
-                    }
-            
-                    // Log login activity
-                    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress;
-                    await LoginActivity.create({
-                        userId: user._id,
-                        name: user.name,
-                        email: user.email,
-                        username: user.username,
-                        ipAddress: ipAddress,
-                    });
-            
-                    return { id: user._id, ...user.toObject() };
-                }
-            
+
                 // Admin hardcoded credentials check
                 const adminId = new mongoose.Types.ObjectId(); // Use a predefined admin ID from environment variable
                 const adminPassword = 'admin123'; // Keep this secure; ideally, it should be hashed
-            
+
                 if (credentials.username === 'Admin' && credentials.password === adminPassword) {
                     const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress;
                     
                     await LoginActivity.create({
-                        userId: adminId, // Use the predefined admin ID
+                        userId: adminId,
                         email: 'admin@pathy.com',
-                        name:'admin',
+                        name: 'admin',
                         username: 'Admin',
                         ipAddress: ipAddress,
-                        role:'admin',
+                        role: 'admin',
                     });
-            
-                    return { id: adminId,name: 'admin', username: 'Admin', email: 'admin@pathy.com', role: 'admin' };
-                } else {
-                    // Update lastLogin if the user exists
-                    user.lastLogin = Date.now();
-                    await user.save();
+
+                    return { id: adminId, name: 'admin', username: 'Admin', email: 'admin@pathy.com', role: 'admin' };
                 }
-                
-            
-                throw new Error("No user found with this username");
+
+                throw new Error("No user found with this username or email");
             },
         }),
         GoogleProvider({
-            name: "google",
+            name: "Google",
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             authorization: {
@@ -106,35 +78,36 @@ export const options = {
                     access_type: "offline",
                 },
             },
+            async authorize(profile) {
+                await mongodbConnect();
+
+                const existingUser = await GoogleUser.findOne({ email: profile.email });
+                if (existingUser) {
+                    // Update last login time
+                    existingUser.lastLogin = new Date();
+                    await existingUser.save();
+                    return { id: existingUser._id, name: existingUser.name, email: existingUser.email, role: 'google_user' }; // Add role if necessary
+                }
+
+                // Create a new user if they don't exist
+                const newUser = new GoogleUser({
+                    name: profile.name,
+                    email: profile.email,
+                    avatar: {
+                        public_id: null,
+                        url: profile.picture,
+                    },
+                    lastLogin: new Date(),
+                });
+                await newUser.save();
+                return { id: newUser._id, name: newUser.name, email: newUser.email, role: 'google_user' }; // Add role if necessary
+            },
         }),
     ],
     callbacks: {
-        async signIn({ user, account, profile }) {
-            await mongodbConnect();
-
-            const existingUser = await GoogleUser.findOne({ email: user.email });
-            if (existingUser) {
-                // Update last login time
-                existingUser.lastLogin = new Date();
-                await existingUser.save();
-                return true;
-            }
-
-            // Create a new user if they don't exist
-            const newUser = new GoogleUser({
-                name: profile.name,
-                email: profile.email,
-                avatar: {
-                    public_id: null,
-                    url: profile.picture,
-                },
-                lastLogin: new Date(),
-            });
-            await newUser.save();
-            return true;
-        },
         async session({ session, user }) {
             session.user.id = user.id; // Add user ID to the session
+            session.user.role = user.role || 'user'; // Ensure role is present
             return session;
         },
     },
