7497bd23ad5051c48bfb176fb433943c7525dce6
T3thr <t.theerapat33@gmail.com>
2024-11-06 16:57:12 +0700

    m

--- data/novels.js
+++ data/novels.js
@@ -3,12 +3,12 @@ export const recommendationText = "เลือกนิยายของคุ
 
 export const novels = [
     { title: "ดอกส้มสีทอง", genre: "รักหวานแหวว",author: "Author 1",imageUrl: "/images/romance/novel1.jpg", description: "Description of ดอกส้มสีทอง" },
-    { title: "โดราเอมอน", genre: "รักหวานแหวว",author: "Author 1",imageUrl: "/images/romance/novel2.jpg", description: "Description of โดราเอมอน" },
-    { title: "ชิงร้อยชิงล้าน", genre: "ตลกขบขัน",author: "Author 1",imageUrl: "/images/comedy/novel1.jpg", description: "Description of ชืงร้อยชิงล้าน" },
-    { title: "หม่ำ เท่ง โหน่ง", genre: "ตลกขบขัน",author: "Author 1",imageUrl: "/images/comedy/novel2.jpg", description: "Description of หม่า เท่ง โหน่ง" },
-    { title: "ธี่หยด", genre: "สยองขวัญ",author: "Author 1",imageUrl: "/images/thriller/novel1.jpg", description: "Description of ธี่หยด" },
-    { title: "ครูบาหมูเด้ง", genre: "สยองขวัญ",author: "Author 1",imageUrl: "/images/thriller/novel2.jpg", description: "Description of ครูบาหมูเด้ง" },
-    { title: "ฟหกด", genre: "แฟนตาซี",author: "Author 1",imageUrl: "/images/fantasy/novel1.jpg", description: "Description of ฟหกด" },
-    { title: "สวัสดี", genre: "แฟนตาซี",author: "Author 1",imageUrl: "/images/fantasy/novel2.jpg", description: "Description of สวัสดี" },
+    { title: "ฺBound to me สยบรักคุณมาเฟีย", genre: "รักหวานแหวว",author: "Author 2",imageUrl: "/images/romance/novel2.jpg", description: "Description of โดราเอมอน" },
+    { title: "ชิงร้อยชิงล้าน", genre: "ตลกขบขัน",author: "Author 3",imageUrl: "/images/comedy/comedy1.jpg", description: "Description of ชืงร้อยชิงล้าน" },
+    { title: "หม่ำ เท่ง โหน่ง", genre: "ตลกขบขัน",author: "Author 4",imageUrl: "/images/comedy/comedy2.jpg", description: "Description of หม่า เท่ง โหน่ง" },
+    { title: "ฆาตกรรมป่าสน", genre: "สยองขวัญ",author: "Author 5",imageUrl: "/images/thriller/thriller1.jpg", description: "Description of ธี่หยด" },
+    { title: "บันทึกเลือด", genre: "สยองขวัญ",author: "Author 6",imageUrl: "/images/thriller/thriller2.jpg", description: "Description of ครูบาหมูเด้ง" },
+    { title: "The Legend of Risenmour มหาศึกไรเซนมัวร์", genre: "แฟนตาซี",author: "Author 7",imageUrl: "/images/fantasy/fantasy1.jpg", description: "Description of ฟหกด" },
+    { title: "The Dead of Nashly เมื่อโลกหลังความตายของแนชลีย์มาเยือน", genre: "แฟนตาซี",author: "Author 8",imageUrl: "/images/fantasy/fantasy2.jpg", description: "Description of สวัสดี" },
   ];
   
