216dd6979d6ad8ad7250b98c6c156aab2fdc4cca
Siwakittikul <159520336+Siwakittikul@users.noreply.github.com>
2024-11-08 00:16:17 +0700

    novels.js

--- data/novels.js
+++ data/novels.js
@@ -2,7 +2,7 @@
 export const recommendationText = "เลือกนิยายของคุณ";
 
 export const novels = [
-    { title: "ดอกส้มสีทอง", genre: "รักหวานแหวว",author: "Author 1",imageUrl: "/images/romance/novel1.jpg", description: "Description of ดอกส้มสีทอง" },
+    { title: "ดอกส้มสีทอง1", genre: "รักหวานแหวว",author: "Author 1",imageUrl: "/images/romance/novel1.jpg", description: "Description of ดอกส้มสีทอง" },
     { title: "ฺBound to me สยบรักคุณมาเฟีย", genre: "รักหวานแหวว",author: "Author 2",imageUrl: "/images/romance/novel2.jpg", description: "Description of โดราเอมอน" },
     { title: "ชิงร้อยชิงล้าน", genre: "ตลกขบขัน",author: "Author 3",imageUrl: "/images/comedy/novel1.jpg", description: "Description of ชืงร้อยชิงล้าน" },
     { title: "หม่ำ เท่ง โหน่ง", genre: "ตลกขบขัน",author: "Author 4",imageUrl: "/images/comedy/novel2.jpg", description: "Description of หม่า เท่ง โหน่ง" },
