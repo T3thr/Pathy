648c20affc1e5b8cf04b77a4c016fa2733d35568
Siwakittikul <159520336+Siwakittikul@users.noreply.github.com>
2024-11-08 01:34:54 +0700

    Update novels.js

--- data/novels.js
+++ data/novels.js
@@ -4,11 +4,12 @@ export const recommendationText = "เลือกนิยายของคุ
 export const novels = [
     { title: "ดอกส้มสีทอง1", genre: "รักหวานแหวว",author: "Author 1",imageUrl: "/images/romance/novel1.jpg", description: "Description of ดอกส้มสีทอง" },
     { title: "ฺBound to me สยบรักคุณมาเฟีย", genre: "รักหวานแหวว",author: "Author 2",imageUrl: "/images/romance/novel2.jpg", description: "Description of โดราเอมอน" },
-    { title: "ชิงร้อยชิงล้าน", genre: "ตลกขบขัน",author: "Author 3",imageUrl: "/images/comedy/novel1.jpg", description: "Description of ชืงร้อยชิงล้าน" },
-    { title: "หม่ำ เท่ง โหน่ง", genre: "ตลกขบขัน",author: "Author 4",imageUrl: "/images/comedy/novel2.jpg", description: "Description of หม่า เท่ง โหน่ง" },
-    { title: "ฆาตกรรมป่าสน", genre: "สยองขวัญ",author: "Author 5",imageUrl: "/images/thriller/thriller1.jpg", description: "Description of ธี่หยด" },
-    { title: "บันทึกเลือด", genre: "สยองขวัญ",author: "Author 6",imageUrl: "/images/thriller/thriller2.jpg", description: "Description of ครูบาหมูเด้ง" },
-    { title: "The Legend of Risenmour มหาศึกไรเซนมัวร์", genre: "แฟนตาซี",author: "Author 7",imageUrl: "/images/fantasy/fantasy1.jpg", description: "Description of ฟหกด" },
-    { title: "The Dead of Nashly เมื่อโลกหลังความตายของแนชลีย์มาเยือน", genre: "แฟนตาซี",author: "Author 8",imageUrl: "/images/fantasy/fantasy2.jpg", description: "Description of สวัสดี" },
+    { title: "ดอกส้มสีทอรักในสายลม", genre: "รักหวานแหวว",author: "Author 3",imageUrl: "/images/romance/novel3.jpeg", description: "Description of ดอกส้มสีทอรักในสายลม" },
+    { title: "ชิงร้อยชิงล้าน", genre: "ตลกขบขัน",author: "Author 4",imageUrl: "/images/comedy/novel1.jpg", description: "Description of ชืงร้อยชิงล้าน" },
+    { title: "หม่ำ เท่ง โหน่ง", genre: "ตลกขบขัน",author: "Author 5",imageUrl: "/images/comedy/novel2.jpg", description: "Description of หม่า เท่ง โหน่ง" },
+    { title: "ฆาตกรรมป่าสน", genre: "สยองขวัญ",author: "Author 6",imageUrl: "/images/thriller/thriller1.jpg", description: "Description of ธี่หยด" },
+    { title: "บันทึกเลือด", genre: "สยองขวัญ",author: "Author 7",imageUrl: "/images/thriller/thriller2.jpg", description: "Description of ครูบาหมูเด้ง" },
+    { title: "The Legend of Risenmour มหาศึกไรเซนมัวร์", genre: "แฟนตาซี",author: "Author 8",imageUrl: "/images/fantasy/fantasy1.jpg", description: "Description of ฟหกด" },
+    { title: "The Dead of Nashly เมื่อโลกหลังความตายของแนชลีย์มาเยือน", genre: "แฟนตาซี",author: "Author 9",imageUrl: "/images/fantasy/fantasy2.jpg", description: "Description of สวัสดี" },
   ];
   
