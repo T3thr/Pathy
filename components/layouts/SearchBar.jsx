6efcfd19e707b688259a494c5d5e3d79dab4a271
T3thr <t.theerapat33@gmail.com>
2024-11-08 09:04:59 +0700

    searchbar

--- components/layouts/SearchBar.jsx
+++ components/layouts/SearchBar.jsx
@@ -122,31 +122,36 @@ export default function SearchBar() {
                     transition: 'opacity 0.3s ease-in-out, transform 0.3s ease-in-out',
                 }}
             >
-                <div className="flex justify-center items-center w-full relative space-x-4">
-                    {/* Close button moved to the left side of the search input */}
-                    <button
-                        onClick={handleCloseButtonClick}  // Handle close button click separately
-                        className="flex mt-4 transform -translate-y-1/2 p-2 rounded-full text-gray-500 hover:text-gray-700 hover:bg-indigo-100 transition-colors"
-                    >
-                        <FaTimesCircle size={24} />
-                    </button>
+                <div className="flex justify-center items-center w-full  space-x-4">
 
+                    <div className='relative '>
+                                            {/* Search button locked to the left side of the search bar */}
+                    <button
+                        type="submit"
+                        className="absolute top-2 mt-4  left-56 transform -translate-y-1/2 px-4 py-3 text-white bg-indigo-600 rounded-e-3xl hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 transition-all"
+                    >
+                        ค้นหา
+                    </button>
                     {/* Search input */}
                     <input
-                        className="flex border w-96 mb-5 text-gray-900 border-gray-300 bg-gray-50 rounded-full py-2 px-8 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-all"
+                        className="flex border w-72 mb-4 h-12  text-gray-900 border-gray-300 bg-gray-50 rounded-full py-2 px-2 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-all"
                         type="text"
                         placeholder="ค้นหานิยาย..."
                         value={keyword}
                         onChange={(e) => setKeyword(e.target.value)}
                         onKeyDown={handleKeyPress} // Handle Enter key press
                     />
-                    {/* Search button locked to the left side of the search bar */}
+
+                    </div>
+
+                    {/* Close button moved to the left side of the search input */}
                     <button
-                        type="submit"
-                        className="flex justify-end relative right-28px mt-2 top-1 transform -translate-y-1/2 px-6 py-2 text-white bg-indigo-600 rounded-full hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 transition-all"
+                        onClick={handleCloseButtonClick}  // Handle close button click separately
+                        className="mt-4 transform -translate-y-1/2 p-2 rounded-full text-gray-500 hover:text-gray-700 hover:bg-indigo-100 transition-colors"
                     >
-                        ค้นหา
+                        <FaTimesCircle size={24} />
                     </button>
+
                 </div>
 
             {/* Search history container, positioned below the search bar */}
@@ -154,16 +159,16 @@ export default function SearchBar() {
                 <div
                     className={`z-40 absolute transition-opacity duration-700 ease-in-out ${showHistory ? 'opacity-100' : 'opacity-0'}`}
                     style={{
-                        top: '3.5rem', 
+                        top: '3.7rem', 
                         left: '50%', // Center horizontally
-                        transform: 'translateX(-50%)', // Adjust to ensure it's centered
-                        width: 'calc(100% - 2rem)', 
-                        maxWidth: '540px', 
-                        transitionDelay: '300ms',
+                        transform: 'translateX(-59%)', // Adjust to ensure it's centered
+                        width: 'calc(100% - 6rem)', 
+                        maxWidth: '265px', 
+                        transitionDelay: '200ms',
                     }}
                 >
                     {searchHistory.length > 0 && (
-                        <div className="bg-gray-100 p-4 rounded-lg shadow-md w-full"> 
+                        <div className="bg-gray-100 p-4 rounded-b-lg shadow-md w-full"> 
                             <h3 className="text-sm font-semibold text-gray-700">Search History</h3>
                             <ul className="mt-2 space-y-2">
                                 {searchHistory.map((item, index) => (
