d695a9f819c1799c7525e132479ee22b5489dffb
T3thr <t.theerapat33@gmail.com>
2024-11-11 05:52:49 +0700

    m

--- app/novel-maker/page.js
+++ app/novel-maker/page.js
@@ -2,15 +2,19 @@
 'use client';
 import { useSession } from 'next-auth/react';
 import NovelMakerLayout from '@/components/novel-maker/NovelMakerLayout';
-
+import Loading from '@/app/loading'
 
 export default function NovelMakerPage() {
     const { data: session, status } = useSession();
 
+    if (status === "loading") {
+        return <Loading/>
+    }
+
     if (!session || session.user.role !== 'admin') {
         return (
         <div className='flex justify-center items-center min-h-screen'>
-            <div className='text-xl text-red-600'>Access Denied, Addmin Only!!</div>
+            <div className='text-xl text-red-600'>Access Denied, Admin Only!!</div>
         </div>
     )}
     return (
